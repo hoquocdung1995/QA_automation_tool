328e4d5f42933a4ed721da539cdba9ef6f4ec690
Hà Vĩnh Phú <havinhphu188@gmail.com>
2017-06-21 21:52:58 +0700

    initial

---
+++ src/main/java/com/csc/javafresher/qaautomationtool/App.java
@@ -0,0 +1,13 @@
+package com.csc.javafresher.qaautomationtool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
