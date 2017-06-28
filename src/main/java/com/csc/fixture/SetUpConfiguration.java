5af3255dd0425f3cbf7e50d82d88ad555df9ea43
tinvo <trungtin.it.66@gmail.com>
2017-06-28 13:20:20 +0700

    add new configuration.java and Properties.java

--- src/main/java/com/csc/fixture/SetUpConfiguration.java
+++ src/main/java/com/csc/fixture/SetUpConfiguration.java
@@ -2,31 +2,40 @@ package com.csc.fixture;
 
 import org.apache.log4j.Logger;
 
+import com.csc.storage.Configuration;
+
 public class SetUpConfiguration {
 
 	private static org.apache.log4j.Logger log = Logger.getLogger(SetUpConfiguration.class);
+
+	public static Configuration configuration = new Configuration();
 
 	public SetUpConfiguration() {
 		log.info("SetUpConfiguration()");
 	}
 
 	public void setFileConfig(String fileName) {
+		configuration.setFileConfig(fileName);
 		log.info("Set File Config: " + fileName);
 	}
 
 	public void setBrowser(String browser) {
+		configuration.setBrowser(browser);
 		log.info("setBrowser: " + browser);
 	}
 
 	public void setDriver(String driver) {
+		configuration.setDriver(driver);
 		log.info("setDriver: " + driver);
 	}
 
 	public void setTestSlaveMachine(String machineUrl) {
+		configuration.setTestSlaveMachine(machineUrl);
 		log.info("setTestSlaveMachine: " + machineUrl);
 	}
 
 	public void setUrl(String url) {
+		configuration.setUrl(url);
 		log.info("setUrl: " + url);
 	}
 
