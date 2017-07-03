7f0ac7895fdd8dc8641aa643cc584496bfa43e50
Phuoc Nguyen <phuocit1310@gmail.com>
2017-07-03 15:41:40 +0700

    Edit code file Screenshot

--- src/main/java/com/csc/storage/Screenshot.java
+++ src/main/java/com/csc/storage/Screenshot.java
@@ -10,12 +10,19 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import com.csc.fixture.SetUpConfiguration;
+
 
 public class Screenshot {
 
 	public static void screenshot(WebDriver driver) throws IOException {
-		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(scrFile, new File("FitNesseRoot/bin" + Screenshot.date() + ".jpg"));
+		try {
+			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, new File(SetUpConfiguration.configuration.getUrl() + Screenshot.date() + ".jpg"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public static String date(){
