f536d5b21bc122140dad52ebdf7e25894780c376
Phuoc Nguyen <phuocit1310@gmail.com>
2017-07-13 09:15:51 +0700

    test

--- src/main/java/com/csc/javafresher/qaautomationtool/App.java
+++ src/main/java/com/csc/javafresher/qaautomationtool/App.java
@@ -2,6 +2,8 @@ package com.csc.javafresher.qaautomationtool;
 
 
 import com.csc.fixture.InteractWebApplication;
+import com.csc.storage.Log4jMachine;
+import com.csc.storage.SystemLogging;
 
 
 /**
@@ -10,15 +12,26 @@ import com.csc.fixture.InteractWebApplication;
  */
 public class App 
 {
+	public static Log4jMachine log4j = new Log4jMachine();
+	public static void testLog(){
+		SystemLogging.registerLoggingObserver(log4j);
+		SystemLogging.info("hello");
+		SystemLogging.error("zzz");
+		SystemLogging.debug("zzz");
+		SystemLogging.fatal("zzz");
+		SystemLogging.trace("zzz");
+		SystemLogging.warn("zzz");
+	}
     public static void main( String[] args ) throws InterruptedException
     {
-    	InteractWebApplication app = new InteractWebApplication();    	
-        app.iNavigateToPage("https://github.com");
-        app.iClickTheElement("gitSignUp");
-        app.iSetTheInputWithValue("userName", "Tuyendt");
-        app.iSetTheInputWithValue("email", "tuyendt@gmail.com");
-        app.iSetTheInputWithValue("password", "tuyendt123");
-        app.iClickTheElement("submit");
+    	testLog();
+//    	InteractWebApplication app = new InteractWebApplication();    	
+//        app.iNavigateToPage("https://github.com");
+//        app.iClickTheElement("gitSignUp");
+//        app.iSetTheInputWithValue("userName", "Tuyendt");
+//        app.iSetTheInputWithValue("email", "tuyendt@gmail.com");
+//        app.iSetTheInputWithValue("password", "tuyendt123");
+//        app.iClickTheElement("submit");
         
 
     }
